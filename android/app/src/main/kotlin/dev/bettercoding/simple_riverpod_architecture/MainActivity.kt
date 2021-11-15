b30c1ce030464f907901818a273540cdadddb03a
Stefan Galler <galler.stefan@gmail.com>
2021-11-15 09:20:51 +0100

    initial commit

---
+++ android/app/src/main/kotlin/dev/bettercoding/simple_riverpod_architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.bettercoding.simple_riverpod_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:simple_riverpod_architecture/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
